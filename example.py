763a7a460071d74b042e51f3423fbde5502ec245
EldarGazizov <kursus1010@gmail.com>
2022-07-30 17:56:17 +0200

    example for test

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
-from src.usda.api.services import NASSService, MARSService, FASService
 from pathlib import Path
 
-ROOT_DIR_PATH = Path(__name__).absolute().parent
+import pandas as pd
 
+from src.usda.api.services import NASSService, MARSService, FASService
+
+
+ROOT_DIR_PATH = Path(__name__).absolute().parent
+PATH_TO_SAVE_DATA = ROOT_DIR_PATH / "data"
+
+# API_KEY taken after registering on each website
 SETTINGS = {
     "fas": {
         "api_key": "api_key",
@@ -20,24 +26,37 @@ SETTINGS = {
 
 
 def main() -> None:
+
+    # Init USDA APIs services
     nass = NASSService(SETTINGS["nass"]["api_key"])
     fas = FASService(SETTINGS["fas"]["api_key"])
     mars = MARSService(SETTINGS["mars"]["api_key"])
 
-    stat_fas = fas.get(endpoint='/gats/countries')
-    stat_mars = mars.get(endpoint=mars.EndpointAPI.GET_REPORTS)
-    stat_nass = nass.get(endpoints=nass.EndpointAPI.GET_API, params={"commodity_desc":"CORN",
-                                                                     "year__GE": "2012",
-                                                                     "state_alpha":"VA"})
+    # Set endpoints and query params for search and filter
+    commodityCode = 101
+    year = 2012
+    stat_mars = mars.get()
+    stat_fas = fas.get(endpoint=f"/esr/exports/commodityCode/{commodityCode}/allCountries/marketYear/{year}")
+    stat_nass = nass.get(params={"commodity_desc": "CORN", "year__GE": "2018", "state_alpha": "VA"})
 
-    for service, name, stat in zip([nass, fas, mars], ['NASS', 'FAS', 'MARS'], [stat_nass, stat_fas, stat_mars]):
+    # DataFrames container
+    dfs: list[pd.DataFrame] = []
+
+    # Main loop walk through all of services
+    for service, name, stat in zip([nass, mars], ["NASS", "MARS"], [stat_nass, stat_mars]):
         print(f"==========={name}===========")
         print(f"status code: {stat}")
         print(f"url: {service.url}")
-        path = service.to_file(ROOT_DIR_PATH)
+        df = service.get_dataframe()
+        dfs.append(df)
+        print(df.head())
+        path = service.to_file(PATH_TO_SAVE_DATA, df)
         print(f"See: {path}")
         print("==========================")
- 
+
+    # Draw histogram sum value CORNs per year from 2018 to 2022
+    nass.plot(dfs[0], x="year", y="Value")
+
 
 if __name__ == "__main__":
     main()
